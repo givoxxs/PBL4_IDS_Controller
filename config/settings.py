85073af79b70f42e83ce0e690f5c0f8c256d6188
mgh-lilyy <ntkl1710@gmai.com>
2024-11-23 15:00:57 +0700

    reset path of localrules file

--- config/settings.py
+++ config/settings.py
@@ -7,19 +7,12 @@ class Settings:
     APP_TITLE = "Quản trị mạng"
     APP_WIDTH = 600
     APP_HEIGHT = 400
-    load_dotenv()
-    ROOT_PATH = os.getenv("ROOT_PATH") 
-
-    if ROOT_PATH is None:
-        print("Error: ROOT_PATH not found in .env file.")
-
-    LOG_PATH = os.path.join(ROOT_PATH, "alert_csv.txt")
-    RULE_PATH = os.path.join(ROOT_PATH, "local.rules")
-  
+    LOG_PATH = "/var/log/snort/alert_csv.txt"
+    RULE_PATH = "/etc/snort/rules/local.rules"
     
-    # if os.name == 'nt':  # Windows
-    #         LOG_PATH = "assets/alert_csv.txt"
-    #         RULE_PATH = "assets/local.rules"
-    # else:  # Ubuntu
-    #     LOG_PATH = "/var/log/snort/alert_csv.txt"
-    #     RULE_PATH = ""  
+    if os.name == 'nt':  # Windows
+            LOG_PATH = "assets/alert_csv.txt"
+            RULE_PATH = "assets/local.rules"
+    else:  # Ubuntu
+        LOG_PATH = "/var/log/snort/alert_csv.txt"
+        RULE_PATH = "/usr/local/etc/rules/local.rules"  
