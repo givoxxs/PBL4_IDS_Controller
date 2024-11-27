8654d24aef1ee650ec90534384f995a256ce5bf6
mgh-lilyy <ntkl1710@gmai.com>
2024-11-27 19:19:31 +0700

    update config panel, but not completed yet

--- views/panel_config.py
+++ views/panel_config.py
@@ -21,42 +21,56 @@ class PanelConfig(tk.Frame):
         ttk.Button(menu_frame, text="Manage Snort Rules", style="Custom.TButton", command=self.manage_snort_rules).pack(side=tk.LEFT, padx=5)
         ttk.Button(menu_frame, text="Manage UFW Rules", style="Custom.TButton", command=self.manage_ufw_rules).pack(side=tk.LEFT, padx=5)
 
-        # Create the interval frame and text widget, but initially hidden
+        # Create the interval frame
         self.interval_frame = tk.Frame(self)
         self.interval_frame.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
         
+        # Create update interval label & entry
         update_interval_label = ttk.Label(self.interval_frame, text="Update interval (ms): ")
         update_interval_label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
         self.update_interval_entry = ttk.Entry(self.interval_frame)
         self.update_interval_entry.grid(row=0, column=1, sticky=tk.E, padx=5, pady=5)
         self.update_interval_entry.insert(0, str(self.controller.config["update_interval"]))
 
+        # Create update max_items label & entry
         max_items_label = ttk.Label(self.interval_frame, text="Max listbox items: ")
-        max_items_label.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5) #Corrected grid call
-
+        max_items_label.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5) 
         self.max_items_entry = ttk.Entry(self.interval_frame)
-        self.max_items_entry.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5)  #Corrected grid call
+        self.max_items_entry.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5)  
         self.max_items_entry.insert(0, str(self.controller.config["max_listbox_items"]))
         
+        #Create save button
         save_button = ttk.Button(self.interval_frame, text="Save", command=self.save_config)
-        save_button.grid(row=2, column=0, columnspan=2, pady=10)  #Changed row for better spacing
+        save_button.grid(row=2, column=0, columnspan=2, pady=10)  
 
+        #hide interval_frame at initialization
         self.interval_frame.grid_remove()
         
-        # Create the snort rule frame and text widget, but initially hidden
+        # Create the snort rule frame and text widget
         self.changing_snort_frame = tk.Frame(self)
-        self.interval_frame.grid(row=3, column=2, sticky="nsew", padx=50, pady=70)
+        self.changing_snort_frame.grid(row=3, column=2, sticky="nsew", padx=50, pady=70)
 
-    
+        # Create changing_snort label & entry
+        changing_snort_label = ttk.Label(self.changing_snort_frame, text="Update your snort rules: ")
+        changing_snort_label.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+        self.changing_snort_entry = ttk.Entry(self.changing_snort_frame)
+        self.changing_snort_entry.grid(row=0, column=1, sticky=tk.E, padx=5, pady=5)
+        # self.changing_snort_entry.insert(0, str(self.controller.config["update_snort_rules"]))
+
+        #hide changing_snort_frame at initialization
+        self.changing_snort_frame.grid_remove()
+
     def show_change_interval(self):
+        self.changing_snort_frame.grid_remove()
         self.interval_frame.grid() #Show the frame
         print("Changing update interval...")
 
-    def hide_change_interval(self):
+    def manage_snort_rules(self):
+        #remove other frame
         self.interval_frame.grid_remove()
 
-
-    def manage_snort_rules(self):
+        #Show the frame
+        self.changing_snort_frame.grid()
         print("Managing Snort rules...")
     def create_snort_rules(self):
         pass
